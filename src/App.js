d6518794bb6b2e1beda2e2b94695146b3b8e949e
jjangtae <106091201+jjangtae@users.noreply.github.com>
2022-06-05 23:37:18 +0900

    css, module

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <Hello />
-      <div className={styles.box}>Apㅇㅈp</div>
+      <div className={styles.box}>App</div>
     </div>
   );
 }
